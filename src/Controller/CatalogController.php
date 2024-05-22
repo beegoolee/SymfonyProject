350fba09633e8c209f3a59f9247bd43bc70838fe
M_Struch <mihailstruchkov@gmail.com>
2024-05-22 18:55:21 +0300

    added yarn, template minor corrections

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     #[Route('/catalog/', 'Catalog main')]
     public function catalogMain(){
         return $this->render('catalog/catalog_main.html.twig');
     }
 
+    /**
+     * @param $section_code
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     #[Route('/catalog/{section_code}', 'Catalog section')]
-    public function catalogSection($section_code){
+    public function catalogSection($section_code,  loggerInterface $logger){
         $someText = ['qwe', 'afawfaw', 'awfg23c1f'];
+
+        $logger->info(sprintf('Opened section with code %s', $section_code));
 
         return $this->render('catalog/catalog_section.html.twig', [
             'section' => $section_code,
